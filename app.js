74db394fa81f4d5de697f8ca9d31f6bd3729e968
Steve Marchal <steve.marchal88@gmail.com>
2021-08-02 15:41:39 +0200

    images dans gitignore

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const mongoose = require('mongoose');
 /* mongoose permet de se connecter à la data base Mongo Db */
 
 const path = require('path');
-/*  */
 
 const helmet = require('helmet'); 
 /* helmet est un module Node.js qui aide à sécuriser les applications «express» en définissant divers en-têtes HTTP.
@@ -19,8 +18,7 @@ const rateLimit = require("express-rate-limit");
 /* Le module Express-rate-limit est un middleware pour Express qui est utilisé pour limiter les demandes répétées aux API publiques 
 et / ou aux points de terminaison tels que la réinitialisation de mot de passe. En limitant le nombre de requêtes au serveur, 
 nous pouvons empêcher le déni de service ( DoS ) attaque . C'est le type d'attaque dans lequel le serveur est inondé de demandes 
-répétées le rendant indisponible pour ses utilisateurs prévus et le fermant finalement.
-*/
+répétées le rendant indisponible pour ses utilisateurs prévus et le fermant finalement. */
 
 const sauceRoutes = require('./routes/sauce'); // importation des routes pour les sauces.
 const userRoutes = require('./routes/user'); // importation des routes pour les utilisateurs.
